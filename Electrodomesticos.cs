e376048a24423d6b1ecbbdce24b41ff02eec9e5a
lpc05 <44166280+lpc05@users.noreply.github.com>
2018-10-15 16:20:37 -0300

    Clase Heladera y correcion de la clase Electrodomesticos

---
+++ Electrodomesticos.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Electrodomesticos
+    {
+        private String color;
+        private String consumoenergetico;
+        private float precio;
+        private float peso;
+
+        public Electrodomesticos()
+        {
+            color = "blanco";
+            consumoenergetico = "F";
+            peso = 5;
+            precio = 0;
+        }
+        public string Getcolor()
+        {
+            return this.color;
+        }
+        public void Setcolor(string color)
+        {
+            this.color = color;
+        }
+        public string Getconsumoenergito()
+        {
+            return this.consumoenergetico;
+        }
+        public void Setconsumoenergetico(string consumoenergito)
+        {
+            this.consumoenergetico = consumoenergito;
+        }
+        public float Getprecio()
+        {
+
+            return this.precio;
+        }
+        public void Setprecio(float precio)
+        {
+            this.precio = precio;
+        }
+        public float Getpeso()
+        {
+            return this.peso;
+        }
+        public void Setpeso(float peso)
+        {
+            this.peso = peso;
+
+        }
+    }
+}
---
+++ Heladera.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Heladera:Electrodomesticos
+    {
+        private string Tipoderefrigerador;
+        private int Capacidaddelrefrigerador;
+        public Heladera()
+        {
+            Tipoderefrigerador = "Congelador";
+            Capacidaddelrefrigerador = 40;
+        }
+        public string GetTipoderefrigerador()
+        {
+            return Tipoderefrigerador;
+        }
+        public int GetCapacidaddelrefrigerador()
+        {
+            return Capacidaddelrefrigerador;
+        }
+        public void SetTipoderefrigerador(string n)
+        {
+            Tipoderefrigerador = n;
+        }
+        public void SetCapacidaddelrefrigerador(int n)
+        {
+            Capacidaddelrefrigerador = n;
+        }
+        public float GetPrecioFinal(float n)
+        {
+            if(Capacidaddelrefrigerador>50)
+            {
+                if(Tipoderefrigerador=="Frigorifico")
+                {
+                    return n + 60;
+                }
+                else
+                {
+                    return n + 100;
+                }
+            }
+            return n;
+        }
+    }
+}
